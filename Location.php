c0b9cd5fd75cb9e7de43e2fb8ae80a2e004271c1
AzizKhoudi <47637703+AzizKhoudi@users.noreply.github.com>
2021-03-04 04:56:46 +0100

    Add files via upload

---
+++ Location.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LocationRepository::class)
+ */
+class Location
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=VeloAl::class, cascade={"persist", "remove"})
+     */
+    private $idvelol;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datedeb;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datefin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdvelol(): ?VeloAl
+    {
+        return $this->idvelol;
+    }
+
+    public function setIdvelol(?VeloAl $idvelol): self
+    {
+        $this->idvelol = $idvelol;
+
+        return $this;
+    }
+
+    public function getDatedeb(): ?\DateTimeInterface
+    {
+        return $this->datedeb;
+    }
+
+    public function setDatedeb(\DateTimeInterface $datedeb): self
+    {
+        $this->datedeb = $datedeb;
+
+        return $this;
+    }
+
+    public function getDatefin(): ?\DateTimeInterface
+    {
+        return $this->datefin;
+    }
+
+    public function setDatefin(\DateTimeInterface $datefin): self
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
